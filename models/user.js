86c207a6e9a873fd881577177c643fa2480e945f
sumin <suminshin211@gmail.com>
2024-06-27 16:35:00 +0900

    파이널 프로젝트 2일차

--- models/user.js
+++ models/user.js
@@ -7,27 +7,26 @@ class User extends Model {
     static initiate(sequelize) {
         return FRCT_user_tbl.init({
             // 식별키
-            FRCT_user_id:{type:DataTypes.INTEGER, autoIncrement:true, allowNull:false},
-            FRCT_user_pwd:{type:DataTypes.STRING(20), allowNull:false},
-            FRCT_user_name:{type:DataTypes.STRING(20), allowNull:false},
-            FRCT_user_gender:{type:Sequelize.ENUM('male', 'female'),
+            FRCT_user_id:{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true, allowNull:false},
+            FRCT_user_email:{type:DataTypes.STRING(500), unique: unique, allowNull:false},
+            FRCT_user_name:{type:DataTypes.STRING(50), allowNull:false},
+            FRCT_user_password:{type:DataTypes.STRING(50), allowNull:false},
+            FRCT_user_gender:{type:DataTypes.ENUM('male', 'female'),
                 allowNull: false,
                 defaultValue: 'male'},
-            FRCT_user_hpNum:{type:DataTypes.STRING(40), allowNull:false},
-            FRCT_user_email:{type:DataTypes.STRING(40), primaryKey:true, allowNull:false},
-            FRCT_user_grade_code:{type:Sequelize.ENUM('personal', 'business'),
+            FRCT_user_hpNum:{type:DataTypes.STRING(13), allowNull:false},
+            FRCT_user_profile_txt:{type:DataTypes.TEXT, allowNull:true},
+            FRCT_user_profile_picture:{type:DataTypes.STRING(500), allowNull:true}, // url
+            FRCT_user_profile_portfolio:{type:DataTypes.TEXT, allowNull:true},
+            FRCT_user_zipcode:{type:DataTypes.STRING(10), allowNull:true},
+            FRCT_user_address:{type:DataTypes.STRING(255), allowNull:true},
+            FRCT_user_address_detail:{type:DataTypes.STRING(255), allowNull:true},
+            FRCT_user_grade_code:{type:DataTypes.ENUM('personal', 'company'),
                 allowNull: false,
                 defaultValue: 'personal'},
-            FRCT_user_zipcode:{type:DataTypes.STRING(40), allowNull:false},
-            FRCT_user_address:{type:DataTypes.STRING(40), allowNull:false},
-            FRCT_user_address_detail:{type:DataTypes.STRING(100), allowNull:false},
-            FRCT_user_del_yn:{type:DataTypes.STRING(40), allowNull:false},
-            FRCT_user_profile_txt:{type:DataTypes.STRING(100), allowNull:false},
-            FRCT_user_profile_picture:{type:DataTypes.STRING, allowNull:false}, // x, url
-            FRCT_user_profile_portfolio:{type:DataTypes.STRING(40), allowNull:false}, // x
-            FRCT_user_cash:{type:DataTypes.DECIMAL, allowNull:false},
-            FRCT_user_created_at:{type:DataTypes.DATE, allowNull:false},
-            FRCT_user_updated_at:{type:DataTypes.DATE, allowNull:false},
+            FRCT_is_deleted:{type:DataTypes.BOOLEAN, default:false},
+            FRCT_user_created_at:{type:DataTypes.DATE, allowNull:true},
+            FRCT_user_updated_at:{type:DataTypes.DATE, allowNull:true},
         },{sequelize, modelName:"User", tableName:"FRCT_user_tbl", timestamps:false})
     }
     static associate(db){
